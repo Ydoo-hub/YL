6d1f3db3379c5c60b6b11200c9567bdcb67b41d7
Ydoo-hub <43613589@qq.com>
2020-03-19 22:37:31 +0800

    '3-19'

--- 正则.js
+++ 正则.js
@@ -1,2 +1,23 @@
+let str = "zhulaoshiShiGEDAshABI,对的！！zanTONG";
+str = str.replace(/[a-zA-Z]/g,content=>{
+    //=>content 就是每次匹配到的结果
+    //验证是否为大写字母：把字母转化成大写后和原来对比，一样的话原来就是大写，不一样则是小写
+    // content.toLowerCase() === content?
+    //或者使用ASCII的大写字母的取值范围进行判断A-65 Z-90
+    //content.charCodeAt()>=65&&content.charCodeAt()<=90
+    //'A'.charCodeAt() 可以查看ASCII的值
 
 
+    return content.toLowerCase() === content? content.toUpperCase():content.toLowerCase();
+
+
+})
+console.log('A'.charCodeAt());
+
+console.log('Z'.charCodeAt());
+
+console.log('z'.toUpperCase());
+
+console.log('A'.toLowerCase());
+
+console.log(str)
