d81e96ff703bd96904301164ae6145bfc543d278
Ydoo-hub <43613589@qq.com>
2020-03-22 21:06:04 +0800

    '验证url'

--- 基础.js
+++ 基础.js
@@ -1,24 +1,17 @@
-//1
-var a={} ,b ='123',c = 123;
-a[b] = 'b';  //对象中key值 数字123和字符串"123" 是同一个
-a[c] = 'c';  //所以 a[c] 将  a[b] 覆盖了
-console.log(a[b])  //c
+/*
+*  在输入框中如何判断输入的是一个正确的网站，例如：用户输入一个字符串，验证是否符合url格式
+*
+* */
 
-//2
-var a={} ,b =Symbol('123'),c = Symbol(123);
-//Symbol 创建出来的的值是唯一的           NaN!==NaN(这个也是唯一的)
-//typeof Symbol('asd')  =>"symbol"
-a[b] = 'b';
-a[c] = 'c';
-console.log(a[b]);
+let str = "https://www.baidu.com.cn/index?x=100&y=300#qwe";
+//用正则写url表达式
+let reg = /^(?:(http|https|ftp):\/\/)?((?:[\w-]+\.)+[a-z0-9]+)((?:\/[^/?#]*)+)?(\?[^#]+)?(#.+)?$/i;
+//=> url 格式
+//1.协议：http https  ftp
+//2.域名 www.zbaidu.com www.****.cn  kbs.sports.qq.com  kbs.sport.qq.con
+//3.请求路径 /index.html  /stu/index.html
+//          /stu/
+//4.问号传参  ?****=***&***=****
+//5.哈希值  #****
 
-//3
-var a={} ,b ={key:'123'},c = {key:'456'};
-a[b] = 'b';
-a[c] = 'c';
-console.log(a[b]) ; //c
-//1.对象（数组）属性名不能是一个对象（遇到对象属性名会转化成字符串）
-//obj={}  arr =[123,321] obj[arr] = 'y' obj=>{'123,321':y}
-//2.普通对象.toString()，调取的是Object。prototype上的方法（这个是根据数据类型检测的）
-//obj={}  obj.toString => "[object Object]"
-//obj[b] = 'b' => obj["[object Object]"] = 'b'
+console.log(reg.exec(str));
