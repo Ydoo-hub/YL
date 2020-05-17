624de610981234c439e0bcc61667dda32bc6a123
ydo <43613589@qq.com>
2020-05-17 22:54:25 +0800

    5-17

---
+++ typescript/5-16.ts
@@ -0,0 +1,12 @@
+//定义类型
+var A: string;
+//A = 1 这个时候就会报错 ，判断到A的那个不是字符串
+A = 'hello tsc' //这个时候定义就没问题 
+var input = document.querySelectorAll('input');
+var button = document.querySelector('button');
+
+button.onclick = function(){
+    console.log("点击了")
+    var result:number = Number(input[0].value)+Number(input[1].value);
+    alert(result);
+}
