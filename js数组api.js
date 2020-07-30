2f404f553381e9574e054ce917e78a0da15f8183
ydo <43613589@qq.com>
2020-07-30 10:52:00 +0800

    'js数组api'

---
+++ js数组api.js
@@ -0,0 +1,82 @@
+// js中数组的api整理
+ //join(), sort(), slice(), splice(), concat(), reverse(), push()+pop(), shift()+unshift(), forEach(), map(), some(), every(), filter(), reduce()+reduceRight(), indexOf()+lastIndexOf(),
+ //Array.form(), Array.of(), copyWithin(), find()+findIndex(), fill(), entries()+keys()+values(), includes()
+
+
+//1.join()
+//join(separator): 将数组的元素组起一个字符串，以separator为分隔符，省略的话则用默认用逗号为分隔符，该方法只接收一个参数：即分隔符。(不改变原数组)
+
+var arr = [1,2,3,4];
+console.log(arr.join()); //1,2,3,4
+console.log(arr.join("/")); //1/2/3/4
+
+//通过join()方法可以实现重复字符串，只需传入字符串以及重复的次数，就能返回重复后的字符串，函数如下：
+
+function repeatString(str, n) {
+    return new Array(n + 1).join(str);
+}
+console.log(repeatString("abc", 3)); // abcabcabc
+
+
+//2.sort()
+//sort()：按升序排列数组项——即最小的值位于最前面，最大的值排在最后面。
+//在排序时，sort()方法会调用每个数组项的 toString()转型方法，然后比较得到的字符串，以确定如何排序。即使数组中的每一项都是数值， sort()方法比较的也是字符串，因此会出现以下的这种情况：
+
+var arr1 = ["d", "a", "c", "b"];
+console.log(arr1.sort()); // ["a", "b", "c", "d"]
+arr2 = [11, 24, 41, 3];
+console.log(arr2.sort()); // [11, 24, 3, 41]
+console.log(arr2); // [13, 24, 3, 51](原数组被改变)
+
+// 为了解决上述问题，sort()方法可以接收一个比较函数作为参数，以便我们指定哪个值位于哪个值的前面。比较函数接收两个参数，如果第一个参数应该位于第二个之前则返回一个负数，如果两个参数相等则返回 0，
+//如果第一个参数应该位于第二个之后则返回一个正数。以下就是一个简单的比较函数：
+
+function compare(a, b) {
+    return a-b
+}
+arr2 = [11, 24, 41, 3];
+console.log(arr2.sort(compare)); // [3, 11, 24, 41]
+// 或者
+console.log(arr2.sort((a,b)=>{return a-b}));
+// 如果需要通过比较函数产生降序排序的结果，只要交换比较函数返回的值return b-a即可。
+
+// 3.slice()
+//slice()：返回从原数组中指定开始下标到结束下标之间的项组成的新数组。slice()方法可以接受一或两个参数，即要返回项的起始和结束位置。在只有一个参数的情况下， 
+//slice()方法返回从该参数指定位置开始到当前数组末尾的所有项。如果有两个参数，该方法返回起始和结束位置之间的项——但不包括结束位置的项。
+var arr = [1,3,5,7,9,11];
+var arrCopy = arr.slice(1);
+var arrCopy2 = arr.slice(1,4);
+var arrCopy3 = arr.slice(1,-2);
+var arrCopy4 = arr.slice(-4,-1);
+console.log(arr); //[1, 3, 5, 7, 9, 11](原数组不变)
+console.log(arrCopy); //[3, 5, 7, 9, 11]
+console.log(arrCopy2); //[3, 5, 7]
+console.log(arrCopy3); //[3, 5, 7]
+console.log(arrCopy4); //[5, 7, 9]
+
+// arrCopy只设置了一个参数，也就是起始下标为1，所以返回的数组为下标1（包括下标1）开始到数组最后。 
+// arrCopy2设置了两个参数，返回起始下标（包括1）开始到终止下标（不包括4）的子数组。 
+// arrCopy3设置了两个参数，终止下标为负数，当出现负数时，将负数加上数组长度的值（6）来替换该位置的数，因此就是从1开始到4（不包括）的子数组。 
+// arrCopy4中两个参数都是负数，所以都加上数组长度6转换成正数，因此相当于slice(2,5)。
+
+
+// 4.splice()
+// splice()：很强大的数组方法，它有很多种用法，可以实现删除、插入和替换。
+// 删除：可以删除任意数量的项，只需指定 2 个参数：要删除的第一项的位置和要删除的项数。例如， splice(0,2)会删除数组中的前两项。
+// 插入：可以向指定位置插入任意数量的项，只需提供 3 个参数：起始位置、 0（要删除的项数）和要插入的项。例如，splice(2,0,4,6)会从当前数组的位置 2 开始插入4和6。
+// 替换：可以向指定位置插入任意数量的项，且同时删除任意数量的项，只需指定 3 个参数：起始位置、要删除的项数和要插入的任意数量的项。插入的项数不必与删除的项数相等。
+// 例如，splice (2,1,4,6)会删除当前数组位置 2 的项，然后再从位置 2 开始插入4和6。
+
+//splice()方法始终都会返回一个数组，该数组中包含从原始数组中删除的项，如果没有删除任何项，则返回一个空数组。
+var arr = [1,3,5,7,9,11];
+var arrRemoved = arr.splice(0,2);
+console.log(arr); //[5, 7, 9, 11]
+console.log(arrRemoved); //[1, 3]
+var arrRemoved2 = arr.splice(2,0,4,6);
+console.log(arr); // [5, 7, 4, 6, 9, 11]
+console.log(arrRemoved2); // []
+var arrRemoved3 = arr.splice(1,1,2,4);
+console.log(arr); // [5, 2, 4, 4, 6, 9, 11]
+console.log(arrRemoved3); //[7]
+
+
