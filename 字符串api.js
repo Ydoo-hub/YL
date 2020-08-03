283d3c4752fc07c20275837ef22733aefd1b50b0
ydo <43613589@qq.com>
2020-08-03 20:32:36 +0800

    '字符串api'

--- 字符串api.js
+++ 字符串api.js
@@ -25,3 +25,47 @@ String.raw
 // 操作相关
 chartAt
 
+// 类似字符串索引
+
+slice/concat
+
+// 截取/合并，和数组同名API用法相同
+
+repeat
+
+// 重复构造字符串
+
+replace
+
+// 替换,源可以是函数，目标可以是regex
+
+split
+
+// 分割字符串成数组，和数组的join对应
+
+substr/substring
+
+// 截取子串，注意substr第二个参数是长度
+
+toLowerCase/toUpperCase
+
+// 转大小写，在需要模糊比较字符串相等的时候经常用到
+
+trim/trimLeft/trimRight
+
+去除空串/去除左空串/去除右空串
+
+padStart/padEnd
+
+// 填充字符串至期望长度
+
+// 编码相关
+fromCharCode/fromCodePoint
+
+// Unicode码转字符/编码点转字符串
+
+charCodeAt/codePointAt
+
+// 字符转Unicode/编码点
+
+
